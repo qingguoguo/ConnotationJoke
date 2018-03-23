27138280b150117af0694b89927c488d67bfb182
qingguoguo <350051556@qq.com>
2018-03-23 18:06:00 +0800

    BaseActivity提交

--- app/src/main/java/com/qingguoguo/connotationjoke/MainActivity.java
+++ app/src/main/java/com/qingguoguo/connotationjoke/MainActivity.java
@@ -1,32 +1,43 @@
 package com.qingguoguo.connotationjoke;
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.connotationjoke.qingguoguo.baselibrary.ioc.CheckNet;
 import com.connotationjoke.qingguoguo.baselibrary.ioc.OnClick;
 import com.connotationjoke.qingguoguo.baselibrary.ioc.ViewById;
-import com.connotationjoke.qingguoguo.baselibrary.ioc.ViewUtils;
+
+import connotationjoke.qingguoguo.com.framelibrary.BaseSkinActivity;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseSkinActivity {
 
     @ViewById(R.id.test_tv)
     private TextView mTextView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ViewUtils.inject(this);
-        mTextView.setText("哈哈哈,注入成功");
+    protected void initTitle() {
+
+    }
+
+    @Override
+    protected void initView() {
+        mTextView = viewById(R.id.test_tv);
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected int getLayoutID() {
+        return R.layout.activity_main;
     }
 
     @CheckNet
-    @OnClick({R.id.test_tv,R.id.test_iv})
+    @OnClick({R.id.test_tv, R.id.test_iv})
     private void testOnclick() {
-        Toast.makeText(this,"哈哈哈,注入事件成功",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "哈哈哈,注入事件成功", Toast.LENGTH_SHORT).show();
     }
 }
