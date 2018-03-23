32552cb13f420ce62d3cfd434d20f6cbfc7ea59d
qingguoguo <350051556@qq.com>
2018-03-23 18:07:00 +0800

    BaseActivity提交

---
+++ baselibrary/src/main/java/com/connotationjoke/qingguoguo/baselibrary/base/BaseActivity.java
@@ -0,0 +1,53 @@
+package com.connotationjoke.qingguoguo.baselibrary.base;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.connotationjoke.qingguoguo.baselibrary.ioc.ViewUtils;
+
+/**
+ * 作者:qingguoguo
+ * 创建日期：2018/3/23 on 16:44
+ * 描述:
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //设置布局
+        setContentView(getLayoutID());
+        ViewUtils.inject(this);
+        //初始化头部
+        initTitle();
+        //初始化界面
+        initView();
+        //初始化数据
+        initData();
+    }
+
+    protected abstract void initTitle();
+
+    protected abstract void initView();
+
+    protected abstract void initData();
+
+    @LayoutRes
+    protected abstract int getLayoutID();
+
+    protected void startActivity(Class<?> clazz) {
+        Intent intent = new Intent(this, clazz);
+        startActivity(intent);
+    }
+
+    protected <T extends View> T viewById(@IdRes int id) {
+        return findViewById(id);
+    }
+}
